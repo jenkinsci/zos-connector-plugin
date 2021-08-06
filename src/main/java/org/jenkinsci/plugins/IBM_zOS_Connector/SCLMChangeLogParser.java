e461826855e0b94bbec78c14942ba45aa565b220
candiduslynx <candiduslynx@users.noreply.github.com>
2021-08-06 13:59:35 +0300

    * update to Jenkins 2.222.4 (#16)
    * remove Digester3 altogether
    
    Co-authored-by: Aleksandr Shcherbakov <a-shch@yandex-team.ru>

--- src/main/java/org/jenkinsci/plugins/IBM_zOS_Connector/SCLMChangeLogParser.java
+++ src/main/java/org/jenkinsci/plugins/IBM_zOS_Connector/SCLMChangeLogParser.java
@@ -4,22 +4,25 @@ package org.jenkinsci.plugins.IBM_zOS_Connector;
 import hudson.model.Run;
 import hudson.scm.ChangeLogParser;
 import hudson.scm.RepositoryBrowser;
-import org.apache.commons.digester3.Digester;
+import jenkins.util.xml.XMLUtils;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 /**
  * <h2>SCLMChangeLogParser</h2>
- *
+ * <p>
  * Parser of changelog.xml from SCLM.
  *
  * @author <a href="mailto:candiduslynx@gmail.com">Alexander Shcherbakov</a>
- *
  * @version 1.0
- *
  * @see SCLMChangeLogSet
  * @see LogSet
  * @see ChangeLogParser
@@ -28,59 +31,104 @@ public class SCLMChangeLogParser extends ChangeLogParser {
     /**
      * Parse changelog.xml.
      *
-     * @param build Current build.
-     * @param browser Current browser.
+     * @param build         Current build.
+     * @param browser       Current browser.
      * @param changelogFile changelog.xml
-     *
      * @return SCLMChangeLogSet from changelog.xml.
-     *
      */
     @Override
     public SCLMChangeLogSet parse(Run build, RepositoryBrowser<?> browser,
-                                                            File changelogFile)
-        throws IOException,
-        SAXException
-    {
-        Digester digester = new Digester();
+                                  File changelogFile)
+            throws IOException,
+            SAXException {
+        Element logE;
+        // read file
+        try {
+            InputStreamReader reader = new InputStreamReader(new FileInputStream(changelogFile), StandardCharsets.UTF_8);
+            logE = XMLUtils.parse(reader).getDocumentElement();
+            reader.close();
+        } catch (IOException | SAXException e) {
+            throw new IOException("Failed to parse " + changelogFile, e);
+        }
 
-        digester.setXIncludeAware(false);
+        NodeList logNL = logE.getChildNodes();
+        Node changelog = null;
 
-        if (!Boolean.getBoolean(SCLMChangeLogParser.class.getName() + ".UNSAFE")) {
-            try {
-                digester.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-                digester.setFeature("http://xml.org/sax/features/external-general-entities", false);
-                digester.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-                digester.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-            }
-            catch ( ParserConfigurationException ex) {
-                throw new SAXException("Failed to securely configure Zos changelog parser", ex);
+        for (int i = 0; i < logNL.getLength(); i++) {
+            Node n = logNL.item(i);
+            if (n.getNodeType() == Node.ELEMENT_NODE && n.getNodeName().equals("changelog")) {
+                changelog = n;
+                break;
             }
         }
 
-        // Parse fields.
-        digester.addObjectCreate("*/changelog", LogSet.class);
-        digester.addSetProperties("*/changelog");
-        digester.addObjectCreate("*/changelog/entry", LogSet.Entry.class);
-        digester.addSetProperties("*/changelog/entry");
-        digester.addBeanPropertySetter("*/changelog/entry/date", "changeDate");
-        digester.addBeanPropertySetter("*/changelog/entry/project");
-        digester.addBeanPropertySetter("*/changelog/entry/alternate");
-        digester.addBeanPropertySetter("*/changelog/entry/group");
-        digester.addBeanPropertySetter("*/changelog/entry/type");
-        digester.addBeanPropertySetter("*/changelog/entry/name");
-        digester.addBeanPropertySetter("*/changelog/entry/version");
-        digester.addBeanPropertySetter("*/changelog/entry/userID");
-        digester.addBeanPropertySetter("*/changelog/entry/changeGroup");
-        digester.addBeanPropertySetter("*/changelog/entry/editType");
-        digester.addSetNext("*/changelog/entry", "addEntry");
+        if (changelog == null) {
+            throw new SAXException("File " + changelogFile + " is not a valid changelog file");
+        }
 
-        // Do the actual parsing
-        InputStreamReader reader = new InputStreamReader(new FileInputStream(changelogFile), StandardCharsets.UTF_8);
-        LogSet temp = digester.parse(reader);
-        reader.close();
+        // Have proper node, scan entries.
+        LogSet temp = new LogSet();
+
+        NodeList entries = changelog.getChildNodes();
+        for (int i = 0; i < entries.getLength(); i++) {
+            Node elItem = entries.item(i);
+            if (elItem.getNodeType() != Node.ELEMENT_NODE) {
+                continue;
+            }
+            if (!elItem.getNodeName().equals("entry")) {
+                continue;
+            }
+            Element entryElement = (Element) elItem;
+            LogSet.Entry e = new LogSet.Entry();
+            NodeList entryNL = entryElement.getChildNodes();
+
+            for (int j = 0; j < entryNL.getLength(); j++) {
+                Node item = entryNL.item(j);
+                if (item.getNodeType() != Node.ELEMENT_NODE) {
+                    continue;
+                }
+                Element otherE = (Element) item;
+                String text = otherE.getTextContent();
+                switch (otherE.getTagName()) {
+                    case "date":
+                        e.setChangeDate(text);
+                        break;
+                    case "project":
+                        e.setProject(text);
+                        break;
+                    case "alternate":
+                        e.setAlternate(text);
+                        break;
+                    case "group":
+                        e.setGroup(text);
+                        break;
+                    case "type":
+                        e.setType(text);
+                        break;
+                    case "name":
+                        e.setName(text);
+                        break;
+                    case "version":
+                        e.setVersion(text);
+                        break;
+                    case "userID":
+                        e.setUserID(text);
+                        break;
+                    case "changeGroup":
+                        e.setChangeGroup(text);
+                        break;
+                    case "editType":
+                        e.setEditType(text);
+                        break;
+                    default:
+                        throw new SAXException("Unexpected node " + otherE.getTagName());
+                }
+                temp.addEntry(e);
+            }
+        }
 
         // Convert to SCLMChangeLogSet
-        SCLMChangeLogSet res = new SCLMChangeLogSet(build,browser);
+        SCLMChangeLogSet res = new SCLMChangeLogSet(build, browser);
         res.fromLogSet(temp);
         return res;
     }
